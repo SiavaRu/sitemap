77f37e8bdb4bef180113e41dcf83643db16b4e37
Paul Norman <31554246+CrazyC0d3r@users.noreply.github.com>
2019-02-14 09:30:35 +0000

    1.0.0

--- event/listener.php
+++ event/listener.php
@@ -1,14 +1,18 @@
 <?php
 /**
 *
-* @package phpBB Extension - Sitemap
+* SEO Sitemap
 * @copyright (c) 2016 Jeff Cocking
+* @copyright (c) 2019 Paul Norman (WelshPaul)
 * @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
 *
 */
 
-namespace lotusjeff\sitemap\event;
+namespace welshpaul\sitemap\event;
 
+use phpbb\config\config;
+use phpbb\template\template;
+use phpbb\controller\helper;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
 /**
@@ -22,22 +26,22 @@ class listener implements EventSubscriberInterface
 	/** @var \phpbb\template\template */
 	protected $template;
 
-	/** @var \phpbb\user */
-	protected $user;
+	/** @var \phpbb\controller\helper */
+	protected $helper;
 
 	/**
 	 * Constructor
 	 *
 	 * @param \phpbb\config\config        $config             Config object
 	 * @param \phpbb\template\template    $template           Template object
-	 * @param \phpbb\user                 $user               User object
+	 * @param \phpbb\controller\helper    $helper             Controller helper object
 	 * @access public
 	 */
-	public function __construct(\phpbb\config\config $config, \phpbb\template\template $template, \phpbb\user $user)
+	public function __construct(config $config, template $template, helper $helper)
 	{
 		$this->config = $config;
 		$this->template = $template;
-		$this->user = $user;
+		$this->helper = $helper;
 	}
 
 	/**
@@ -50,8 +54,19 @@ class listener implements EventSubscriberInterface
 	static public function getSubscribedEvents()
 	{
 		return array(
-			'core.page_header'		=> 'lotusjeff_sitemap_set_tpl_data',
+			'core.user_setup'	=> 'load_language_on_setup',
+			'core.page_header'	=> 'welshpaul_sitemap_set_tpl_data',
 		);
+	}
+
+	public function load_language_on_setup($event)
+	{
+		$lang_set_ext = $event['lang_set_ext'];
+		$lang_set_ext[] = array(
+			'ext_name' => 'welshpaul/sitemap',
+			'lang_set' => 'common',
+		);
+		$event['lang_set_ext'] = $lang_set_ext;
 	}
 
 	/**
@@ -60,15 +75,13 @@ class listener implements EventSubscriberInterface
 	 * @return null
 	 * @access public
 	 */
-	public function lotusjeff_sitemap_set_tpl_data()
+	public function welshpaul_sitemap_set_tpl_data()
 	{
 
-		if ($this->config['lotusjeff_sitemap_link'])
+		if ($this->config['welshpaul_sitemap_link'])
 		{
-			$this->user->add_lang_ext('lotusjeff/sitemap', 'common');
-			$sitemap_url = generate_board_url()."/app.php/sitemap/sitemap.xml";
-			$this->template->assign_var('S_LOTUSJEFF_SITEMAP_LINK',$this->config['lotusjeff_sitemap_link']);
-			$this->template->assign_var('LOTUSJEFF_SITEMAP_URL',$sitemap_url);
+			$this->template->assign_var('S_WELSHPAUL_SITEMAP_LINK', $this->config['welshpaul_sitemap_link']);
+			$this->template->assign_var('WELSHPAUL_SITEMAP_URL', $this->helper->route('welshpaul_sitemap_sitemapindex', array(), true, '', \Symfony\Component\Routing\Generator\UrlGeneratorInterface::ABSOLUTE_URL));
 		}
 	}
 }
