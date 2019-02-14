c6af9c7845756f2b70c9bae2f81652c3709d0ada
Paul Norman <31554246+CrazyC0d3r@users.noreply.github.com>
2019-02-14 09:16:57 +0000

    1.0.0

--- acp/sitemap_info.php
+++ acp/sitemap_info.php
@@ -1,27 +1,24 @@
 <?php
 /**
 *
-* @package phpBB Extension - Sitemap
+* SEO Sitemap
 * @copyright (c) 2016 Jeff Cocking
+* @copyright (c) 2019 Paul Norman (WelshPaul)
 * @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
 *
 */
 
-namespace lotusjeff\sitemap\acp;
+namespace welshpaul\sitemap\acp;
 
 class sitemap_info
 {
 	public function module()
 	{
 		return array(
-			'filename'	=> '\lotusjeff\sitemap\acp\sitemap_module',
+			'filename'	=> '\welshpaul\sitemap\acp\sitemap_module',
 			'title'		=> 'ACP_SITEMAP_TITLE',
 			'modes'		=> array(
-				'settings'	=> array(
-					'title' => 'ACP_SITEMAP_SETTINGS',
-					'auth' => 'ext_lotusjeff/sitemap && acl_a_board',
-					'cat' => array('ACP_SITEMAP_TITLE')
-				),
+				'settings'	=> array('title' => 'ACP_SITEMAP_SETTINGS', 'auth' => 'ext_welshpaul/sitemap && acl_a_board', 'cat' => array('ACP_SITEMAP_TITLE')),
 			),
 		);
 	}
