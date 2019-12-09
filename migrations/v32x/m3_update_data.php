bd9312677458f2eb03a1c12959195b6edc3f2456
Paul Norman <paul@cariadcymru.com>
2019-12-09 16:08:39 +0000

    Update m3_update_data.php

--- migrations/v32x/m3_update_data.php
+++ migrations/v32x/m3_update_data.php
@@ -10,7 +10,7 @@
 
 namespace welshpaul\sitemap\migrations\v32x;
 
-class m3_update_config extends \phpbb\db\migration\migration
+class m3_update_data extends \phpbb\db\migration\migration
 {
 	public function effectively_installed()
 	{
