d7f6ee02c287d6e8f655dfa6129c4bf9598a5f96
Paul Norman <paul@cariadcymru.com>
2020-05-12 13:15:21 +0100

    Update m1_initial_config.php

--- migrations/v32x/m1_initial_config.php
+++ migrations/v32x/m1_initial_config.php
@@ -2,8 +2,7 @@
 /**
 *
 * SEO Sitemap
-* @copyright (c) 2016 Jeff Cocking
-* @copyright (c) 2019 Paul Norman (WelshPaul)
+* @copyright (c) 2020 Paul Norman (WelshPaul)
 * @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
 *
 */
@@ -14,25 +13,25 @@ class m1_initial_config extends \phpbb\db\migration\migration
 {
 	static public function depends_on()
 	{
-		return array('\phpbb\db\migration\data\v320\v320');
+		return ['\phpbb\db\migration\data\v320\v320'];
 	}
 
 	public function update_data()
 	{
-		return array(
-			array('config.add', array('welshpaul_sitemap_images', 1)),
-			array('config.add', array('welshpaul_sitemap_sticky_priority', 0.8)),
-			array('config.add', array('welshpaul_sitemap_announce_priority', 0.8)),
-			array('config.add', array('welshpaul_sitemap_global_priority', 0.8)),
-			array('config.add', array('welshpaul_sitemap_forum_exclude', 'a:0:{}')),
-			array('config.add', array('welshpaul_sitemap_forum_threshold', 0)),
-			array('config.add', array('welshpaul_sitemap_link', 1)),
-			array('config.add', array('welshpaul_sitemap_versions', '1.0.0')),
-			array('module.add', array(
+		return [
+			['config.add', ['welshpaul_sitemap_images', 1]],
+			['config.add', ['welshpaul_sitemap_sticky_priority', 0.8]],
+			['config.add', ['welshpaul_sitemap_announce_priority', 0.8]],
+			['config.add', ['welshpaul_sitemap_global_priority', 0.8]],
+			['config.add', ['welshpaul_sitemap_forum_exclude', 'a:0:{}']],
+			['config.add', ['welshpaul_sitemap_forum_threshold', 0]],
+			['config.add', ['welshpaul_sitemap_link', 1]],
+			['config.add', ['welshpaul_sitemap_versions', '1.0.0']],
+			['module.add', [
 				'acp',
 				'ACP_CAT_DOT_MODS',
 				'ACP_SITEMAP_TITLE'
-			)),
-		);
+			]],
+		];
 	}
 }
