e3cf6333c4d220f132ca221d0bf4c7cab35e0f11
Paul Norman <paul@cariadcymru.com>
2020-05-12 13:17:22 +0100

    Create m5_update_data.php

---
+++ migrations/v32x/m5_update_data.php
@@ -0,0 +1,28 @@
+<?php
+/**
+*
+* SEO Sitemap
+* @copyright (c) 2020 Paul Norman (WelshPaul)
+* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+*
+*/
+
+namespace welshpaul\sitemap\migrations\v32x;
+
+class m5_update_data extends \phpbb\db\migration\migration
+{
+	public function effectively_installed()
+	{
+		return isset($this->config['welshpaul_sitemap_versions']) && version_compare($this->config['welshpaul_sitemap_versions'], '1.0.3', '>=');
+	}
+	static public function depends_on()
+	{
+		return ['\welshpaul\sitemap\migrations\v32x\m4_update_data'];
+	}
+	public function update_data()
+	{
+		return [
+			['config.update', ['welshpaul_sitemap_versions', '1.0.3']],
+		];
+	}
+}
