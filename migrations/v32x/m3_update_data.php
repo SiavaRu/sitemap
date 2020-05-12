f49fb28619763bab0364012ae8a31ad280741b2a
Paul Norman <paul@cariadcymru.com>
2020-05-12 13:16:18 +0100

    Update m3_update_data.php

--- migrations/v32x/m3_update_data.php
+++ migrations/v32x/m3_update_data.php
@@ -2,8 +2,7 @@
 /**
 *
 * SEO Sitemap
-* @copyright (c) 2016 Jeff Cocking
-* @copyright (c) 2019 Paul Norman (WelshPaul)
+* @copyright (c) 2020 Paul Norman (WelshPaul)
 * @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
 *
 */
@@ -18,12 +17,12 @@ class m3_update_data extends \phpbb\db\migration\migration
 	}
 	static public function depends_on()
 	{
-		return array('\welshpaul\sitemap\migrations\v32x\m2_initial_module');
+		return ['\welshpaul\sitemap\migrations\v32x\m2_initial_module'];
 	}
 	public function update_data()
 	{
-		return array(
-			array('config.update', array('welshpaul_sitemap_versions', '1.0.1')),
-		);
+		return [
+			['config.update', ['welshpaul_sitemap_versions', '1.0.1']],
+		];
 	}
 }
